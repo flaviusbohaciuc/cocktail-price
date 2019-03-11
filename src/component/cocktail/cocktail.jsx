21120e048e582941dba3c98f8d1190180f525af1
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2019-03-11 22:25:41 +0000

    make use of the async method to get the data

---
+++ src/component/cocktail/cocktail.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3535/';
+
+class Cocktail extends Component{
+    state = {
+        cocktail: []
+    }
+
+    async getCocktail() {
+        let {data} = await axios.get(`${BASE_URL}cocktail`)
+        this.setState({cocktail: data})
+    }
+
+    componentDidMount() {
+        this.getCocktail()
+    }
+    
+    render() {
+        return(
+            <p>{this.state.cocktail.length}</p>
+        )
+    }
+}
+
+export default Cocktail;
